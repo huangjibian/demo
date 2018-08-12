7a56ab551737829dcd7f98dbe7d15d6881d36e74
huangjibian <738516327@qq.com>
2018-08-12 22:54:27 +0800

    aaaa

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
-
+    @RequestMapping("/")
+    String home(){
+    return "hello world";
+    }
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
